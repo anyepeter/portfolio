e071206b64174b8be859fb7205ba3c3810fddec6
anyepeter <petzyrockchendi@gmail.com>
2023-04-29 22:21:17 +0100

    add navbar data

---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return(
+        <>
+        <h1> How are you </h1>
+        </>
+    )
+}
+
+export default Navbar;
---
+++ src/data/navbarData.jsx
@@ -0,0 +1,19 @@
+
+export  const navData = [
+    {
+        name: "portfolio",
+        path: "#projects"
+    },
+    {
+        name: "skills",
+        path: "#skills"
+    },
+    {
+        name: "About",
+        path: "#about"
+    },
+    {
+        name: "Contact",
+        path: "#contact"
+    }
+]
