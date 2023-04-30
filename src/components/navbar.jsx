2498c3ed4a3c80b6175084066588c51f2d7bf4fe
anyepeter <petzyrockchendi@gmail.com>
2023-04-30 20:14:51 +0100

    add desktop navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = () => {
         setMenu(!menu)
     } 
     return(
-        <header className="h-16 w-full bg-black pl-2 pr-4 fixed top-0" >
-        <nav className="h-inherit w-full flex justify-between items-center">
+        <header className="h-16 w-full bg-black pl-2 pr-4 fixed flex items-center justify-center top-0" >
+        <nav className="h-inherit w-full flex justify-between items-center nav-bar">
             <div className="w-22">
                 <img className="w-20 logo" src={logo} />
             </div>
-            <div className="h-inherit flex justify-center items-center z-10">
+            <div className="h-inherit flex justify-center items-center z-10 mobile-menu">
             <div onClick={handleToggle} className="h-10 w-8 flex flex-col gap-y-2 hambeger"> 
                 <span  className={menu ? "hamberlinks-1 spin-1" : "hamberlinks-1"}></span>
                 <span  className={menu ? "" : "hamberlinks-2"}></span>
@@ -26,11 +26,10 @@ const Navbar = () => {
                 {
                   navData.map(item => {
                     return(
-                        <li onClick={handleToggle} 
-                        className={menu ? "link-container active links " : "link-container "}
+                        <li onClick={handleToggle} className="links"
                          key={item.id}>
-                            <a href={item.path}>{item.name}</a>
-                            <div className="nav-item-wrapper"></div></li>
+                            <a className="link-name" href={item.path}>{item.name}</a>
+                        </li>
                     )
                   })
                 }
