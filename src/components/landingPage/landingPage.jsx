ab3c77bf957bace5ff5b5fa2111655d470faacbc
anyepeter <petzyrockchendi@gmail.com>
2023-05-01 23:09:18 +0100

    add backgroung color

--- src/components/landingPage/landingPage.jsx
+++ src/components/landingPage/landingPage.jsx
@@ -1,22 +1,35 @@
-import hero from '../../assets/hero.png'
+import { motion, AnimatePresence } from 'framer-motion';
+import hero from '../../assets/hero-image.png'
 import './landingPage.css';
+
 
 const LandingPage = () => {
     return (
-        <section className="w-full h-full px-4 landing-container">
+        <section className="px-4 landing-container">
             <div className='flex mt-20 landing-section'>
-            <div>
-                <div>
-                    <h1>Hello! I'm Peter</h1>
-                    <h2>
-                        I'm developing functional and interactive web apps
-                    </h2>
+            <div className="intro-container" >
+                <div className='flex flex-col gap-4 '>
+                    <h2 className='text-3xl font-medium'>Peter Chendi</h2>
+                    <AnimatePresence>
+                   <motion.h1 className='text1'
+                   initial={{ opacity: 0 }}
+                 animate={{ opacity: 1 }}
+                 transition={{ ease: [0.17, 0.67, 0.83, 0.67], delay: 1 }}
+                   >
+                        Full-stack developer
+                       </motion.h1>
+                       </AnimatePresence>
                 </div>
                 <div>
-                    As a full stack developermy ex pictureslies in creatingfunctional and interactive user
-                    As a full stack developermy ex pictureslies in creatingfunctional and interactive user
-                    As a full stack developermy ex pictureslies in creatingfunctional and interactive user
-                    As a full stack developermy ex pictureslies in creatingfunctional and interactive user
+               <p className='text-xl leading-6'>Hello! 
+                 I can help you build a product, feature or website.
+                  Take a look at my works. If you like what you see 
+                  and have a project you need coded, don't hesitate 
+                  and contact me.
+
+</p>
+
+
                 </div>
             </div>
             <div>
