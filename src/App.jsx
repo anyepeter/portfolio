f119ce32e8bd4761c92e7baef99929f1ab4d71b0
anyepeter <petzyrockchendi@gmail.com>
2023-05-03 10:20:08 +0100

    add css for projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/navbar'
 import LandingPage from './components/landingPage/landingPage'
+import Project from './components/projects/projects'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
      <Navbar />
      <LandingPage />
+     <Project />
     </>
   )
 }
